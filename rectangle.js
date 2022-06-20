3e5b6874926cbde8179d17f8a47c9d8b102555c0
Your Name <you@example.com>
2022-06-20 16:56:55 +0700

    rectangle

--- rectangle.js
+++ rectangle.js
@@ -20,3 +20,14 @@ let height=result.getheight();
 let area=result.getArea();
 let perimeter=result.getperimeter();
 alert('Chiều rộng = '+ width + ', chiều dài = '+height+', S = '+ area + ', chu vi = '+perimeter);
+
+
+function drawrect(){
+    let canvas= document.getElementById('mycanvas');
+    let ctx =canvas.getContext("2d");
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
+}
+drawrect();
+
