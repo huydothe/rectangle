db4f18e51ffe5436a6377d42f01032921c10dcf3
Your Name <you@example.com>
2022-06-20 15:51:45 +0700

    rectangle

---
+++ rectangle.js
@@ -0,0 +1,22 @@
+let rectangle=function (width,height){
+    this.width= width;
+    this.height=height;
+    this.getWidth=function (){
+        return width;
+    }
+    this.getheight=function (){
+        return height;
+    }
+    this.getArea=function (){
+        return width*height;
+    }
+    this.getperimeter=function (){
+        return (width+height)*2;
+    }
+}
+let result=new rectangle(14,20);
+let width=result.getWidth();
+let height=result.getheight();
+let area=result.getArea();
+let perimeter=result.getperimeter();
+alert('Chiều rộng = '+ width + ', chiều dài = '+height+', S = '+ area + ', chu vi = '+perimeter);
